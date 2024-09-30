e717944e3ea01975eb493041e37619f941146f44
Daniela Parra Virguez <danielaparra1104@gmail.com>
2024-09-30 10:09:39 -0500

    Controller, Service and Sector Repository (User-TypeProperty-State)

---
+++ RELS/Controllers/StateController.cs
@@ -0,0 +1,90 @@
+﻿using RELS.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RELS.Services;
+
+
+namespace RELS.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        private readonly IStateService _StateService;
+
+        public StateController(IStateService stateService)
+        {
+            _StateService = stateService;
+        }
+
+
+        // Get all State
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<State>>> GetAllStates()
+        {
+            var states = await _StateService.GetAllStatesAsync();
+            return Ok(states);
+        }
+
+
+        // Get state by id
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<State>> GetStateById(int id)
+        {
+            var state = await _StateService.GetStateByIdAsync(id);
+            if (state == null) return NotFound();
+
+            return Ok(state);
+        }
+
+
+        // Create a State
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> CreateState([FromForm] State state)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            await _StateService.CreateStateAsync(state);
+            return CreatedAtAction(nameof(GetStateById), new { id = state.Id }, state);
+        }
+
+
+        // Update a State
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> UpdateState(int id, [FromForm] State state)
+        {
+            if (id != state.Id) return BadRequest();
+
+            var existingUser = await _StateService.GetStateByIdAsync(id);
+            if (existingUser == null) return NotFound();
+
+            await _StateService.UpdateStateAsync(state);
+            return NoContent();
+        }
+
+        // Delete a state
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> SoftDeleteState(int id)
+        {
+            var user = await _StateService.GetStateByIdAsync(id);
+            if (user == null) return NotFound();
+
+            await _StateService.SoftDeleteStateAsync(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ RELS/Repositories/UserRepository.cs
@@ -0,0 +1,78 @@
+﻿using RELS.Context;
+using RELS.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace RELS.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task SoftDeleteUserAsync(int id);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly RealEstateDbContext _context;
+
+        public UserRepository(RealEstateDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create User
+        public async Task CreateUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+        }
+        // Get user by Id
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.AsNoTracking()
+            .FirstOrDefaultAsync(s => s.Id == id && !s.IsDeleted);
+
+        }
+        // Get all user
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users
+           .Where(s => !s.IsDeleted) // Avoid deleted items
+           .ToListAsync();
+
+        }
+        // Update User
+        public async Task UpdateUserAsync(User user)
+
+        {
+            try
+            {
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+
+                throw;
+
+            }
+
+        }
+
+        // Delete .user
+
+        public async Task SoftDeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                user.IsDeleted = true;
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
